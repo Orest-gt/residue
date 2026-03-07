22581888a9b8334ddd79dab272212ba075016d31
Orest-gt <orestisgatos@gmail.com>
2026-03-07 18:23:08 +0200

    V4.2.3.post2: Fix syntax error in setup.py and bump post-release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ ext_modules = [
             "src",
         ],
         cxx_std=20,
-        define_macros=[("VERSION_INFO", '"4.2.3.post1"')],
+        define_macros=[("VERSION_INFO", '"4.2.3.post2"')],
         extra_compile_args=["/O2", "/bigobj", "/std:c++20", "/arch:AVX2"] if "win" in sys.platform else ["-O3", "-std=c++20", "-mavx2", "-mfma", "-mpopcnt"],
         extra_link_args=["winmm.lib"] if "win" in sys.platform else [],
     ),
@@ -27,7 +27,7 @@ ext_modules = [
 
 setup(
     name="residue-protocol",
-    version="4.2.3.post1",,
+    version="4.2.3.post2",
     author="PROJECT RESIDUE",
     author_email="orestisgatos@gmail.com",
     description="Bare-Metal AVX2 Inference Shield - V4.2 Reality-Synchronized",
